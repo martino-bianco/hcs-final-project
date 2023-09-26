71c0181c426f5caba049773eaa2b348dfd348e37
Martino-Bianco <martin.g.doerr@gmail.com>
2023-09-26 18:37:33 +0200

    App.vue clean

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,7 @@
 <template>
   <router-view />
-  <div id="cardContainer">
-    <TheCardTemplate
-      v-for="travelItem in travelsData"
-      :key="travelItem.id"
-      :travelData="travelItem"
-    />
-  </div>
 </template>
 
-<script>
-import travelsData from '../public/travels.json'
-
-export default {
-  data() {
-    return {
-      travelsData: []
-    }
-  },
-  mounted() {
-    this.travelsData = travelsData
-  }
-}
-</script>
+<script></script>
 
 <style scoped></style>
