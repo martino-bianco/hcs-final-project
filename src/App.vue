d795c857307935499fd8c5a5ca430b9fc8e77fa8
Martino-Bianco <martin.g.doerr@gmail.com>
2023-09-26 10:21:52 +0200

    cleaning comments

--- src/App.vue
+++ src/App.vue
@@ -10,58 +10,18 @@
 </template>
 
 <script>
-import travelsData from '../public/travels.json' // Adjust the path as needed
+import travelsData from '../public/travels.json'
 
 export default {
   data() {
     return {
-      travelsData: [] // Initialize an empty array to hold the data
+      travelsData: []
     }
   },
   mounted() {
-    this.travelsData = travelsData // Assign the imported JSON data to the component's data property
+    this.travelsData = travelsData
   }
 }
 </script>
 
 <style scoped></style>
-
-<!-- <template>
-  <router-view />
-  <TheMenuBar />
-  <div id="container">
-    <div id="leftColumn">
-      <ThePreviewCards />
-    </div>
-    <div id="rightColumn">
-      <TheMap />
-    </div>
-  </div>
-</template>
-
-<script>
-import TheMap from './components/TheMap.vue'
-import TheMenuBar from './components/TheMenuBar.vue'
-import ThePreviewCards from './components/ThePreviewCards.vue'
-
-export default {
-  components: { TheMenuBar, ThePreviewCards, TheMap }
-}
-</script>
-
-<style scoped>
-#container {
-  display: flex;
-}
-
-#leftColumn {
-  flex: 1;
-  padding: 16px;
-}
-
-#rightColumn {
-  flex: 1;
-  padding: 16px;
-  height: 800px;
-}
-</style> -->
